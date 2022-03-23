7f0d04af068be50002e2398b2725bf10c12f31ef
Livia Lima <liviarchlima@gmail.com>
2022-03-23 20:53:30 -0300

    terceira atividade workshop - modificado

--- atividade_r.R
+++ atividade_r.R
@@ -1,23 +1,11 @@
 ## ATIVIDADE R - LIVIA LIMA
 
-library(tidyverse)
-mtcars
-mtcars %>% select(mpg, cyl, gear)
-
-mtcars %>% filter(cyl >= 6)
 
 install.packages('ggplot2')
-library(ggplot2)
 
-mtcars %>%
-  ggplot(aes(mpg, cyl)) + 
-  geom_area(stat = "bin", fill = 'red', alpha = .5)
+library(ggplot2)
  
 library(tidyverse)
-
-mtcars %>%
-  ggplot(aes(mpg, cyl)) + 
-  geom_area(stat = "bin", fill = 'red', alpha = .5)
 
 mtcars
 
